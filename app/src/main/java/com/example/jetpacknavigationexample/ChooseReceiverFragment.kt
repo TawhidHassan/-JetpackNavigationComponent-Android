a243894350f43aa1460ad0185736335f6d70ec54
sifat hassan <sifat007.sh7@gmail.com>
2021-04-12 13:48:35 +0600

    Pass Data to Destinations with SafeArgs

--- app/src/main/java/com/example/jetpacknavigationexample/ChooseReceiverFragment.kt
+++ app/src/main/java/com/example/jetpacknavigationexample/ChooseReceiverFragment.kt
@@ -2,8 +2,24 @@ package com.example.jetpacknavigationexample
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_choose_receiver.*
 
-class ChooseReceiverFragment : Fragment(R.layout.fragment_choose_receiver)
+class ChooseReceiverFragment : Fragment(R.layout.fragment_choose_receiver){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        btn_next.setOnClickListener{
+
+            val receiverName = et_receiver_name.text.toString()
+
+
+            val action=ChooseReceiverFragmentDirections .actionChooseReceiverFragmentToSendCashFragment(receiverName,400)
+            findNavController().navigate(action)
+        }
+    }
+}
--- app/src/main/java/com/example/jetpacknavigationexample/ViewTransactionsFragment.kt
+++ app/src/main/java/com/example/jetpacknavigationexample/ViewTransactionsFragment.kt
@@ -1,10 +1,22 @@
 package com.example.jetpacknavigationexample
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_view_transactions.*
 
 
-class ViewTransactionsFragment : Fragment(R.layout.fragment_view_transactions)
+class ViewTransactionsFragment : Fragment(R.layout.fragment_view_transactions){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_test.setOnClickListener {
+            val action = ViewTransactionsFragmentDirections
+                .actionViewTransactionsFragmentToSendCashFragment("testname",1000)
+            findNavController().navigate(action)
+        }
+
+    }
+}
