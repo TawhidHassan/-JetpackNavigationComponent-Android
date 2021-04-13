43ccbf18f9192003bc44e343c88980089fefab5a
sifat hassan <sifat007.sh7@gmail.com>
2021-04-13 10:01:59 +0600

    Global Actions with Navigation Component

--- app/src/main/java/com/example/jetpacknavigationexample/MainActivity.kt
+++ app/src/main/java/com/example/jetpacknavigationexample/MainActivity.kt
@@ -34,7 +34,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        return when(item.itemId){
+            R.id.item_about_app ->{
+                val action = MainNavGraphDirections.actionGlobalAboutFragment()
+                navController.navigate(action)
+                return true
+            }
+            else -> item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/example/jetpacknavigationexample/SettingsFragment.kt
+++ app/src/main/java/com/example/jetpacknavigationexample/SettingsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_settings.*
 
 
@@ -18,6 +19,11 @@ class SettingsFragment : Fragment(R.layout.fragment_settings){
             val defaultAmount = et_default_amount.text.toString().toLong()
             SampleData.defaultAmount.value = defaultAmount
         }
+        btn_about_app.setOnClickListener {
+            val action = MainNavGraphDirections.actionGlobalAboutFragment()
+            findNavController().navigate(action)
+        }
+
 
     }
 }
